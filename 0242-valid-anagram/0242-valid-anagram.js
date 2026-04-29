4a73f1aa814d2361aa172eb5d448ce7ab6562afb
NikhilSajan95 <nikhilputhenpurakal@gmail.com>
2026-04-30 00:18:27 +0530

    Time: 13 ms (62.12%), Space: 61.2 MB (9.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,21 +3,15 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
+    if (s.length != t.length) return false    
     let obj = {}
-    for (let i of s){
-        obj[i] = (obj[i] || 0) + 1 
+    for (let i of s) {
+        obj[i] = (obj[i] || 0) + 1
     }
-
-    for (let i of t){
-       if(obj[i]){
-        obj[i] = obj[i] - 1
-        if(obj[i] == 0) delete obj[i]
-       }else 
-       return false   
+    for (let i of t) {
+        if(!obj[i]) return false
+        obj[i]--
     }
-    // console.log(Object.keys(obj).length)
-    if(Object.keys(obj).length) return false
     return true
-    
 };
