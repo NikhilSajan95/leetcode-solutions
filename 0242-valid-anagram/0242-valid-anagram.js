939e65b2d76f06385c682b87d382e8df07bd52f2
NikhilSajan95 <nikhilputhenpurakal@gmail.com>
2026-04-30 00:24:47 +0530

    Add alternative anagram check implementation as comments
    Added commented-out alternative implementation for checking anagrams.

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -15,3 +15,19 @@ var isAnagram = function (s, t) {
     }
     return true
 };
+//  OR
+// if (s.length != t.length)
+//         return false;
+
+//     console.log('a'.charCodeAt(0))
+//     var array = new Array(26).fill(0);
+//     for (let i = 0; i < s.length; i++) {
+//         array[t[i].charCodeAt(0) - 97] ++;
+//         array[s[i].charCodeAt(0) - 97] --;
+//     }
+
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] !== 0) return false;
+//     }
+
+//     return true;
