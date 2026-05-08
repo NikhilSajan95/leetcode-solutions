6525ff06b213fe6f150344016b935ec5741cd872
NikhilSajan95 <nikhilputhenpurakal@gmail.com>
2026-05-08 23:56:29 +0530

    Improve destCity function logic
    Refactor destCity function to use a Set for start cities.

--- 1436-destination-city/1436-destination-city.js
+++ 1436-destination-city/1436-destination-city.js
@@ -3,14 +3,26 @@
  * @return {string}
  */
 var destCity = function(paths) {
-    let path = paths.flat()
-    let map = new Map()
-    for (let i=0; i<path.length; i++){
-        if(i%2 == 0) map.set(path[i],1)
+    // let path = paths.flat()
+    // let map = new Map()
+    // for (let i=0; i<path.length; i++){
+    //     if(i%2 == 0) map.set(path[i],1)
+    // }
+    // for (let i=0; i<path.length; i++){
+    //     if(i%2 !== 0){
+    //         if(!map.has(path[i])) return path[i]
+    //     }
+    // }
+    
+    // OR (best)
+    let startCities = new Set();
+    for (let [from,}
+    to] of paths) {
+        startCities.add(from);
     }
-    for (let i=0; i<path.length; i++){
-        if(i%2 !== 0){
-            if(!map.has(path[i])) return path[i]
-        }
-    }
+
+    for (let [from, to] of paths) {
+        if (!startCities.has(to)) {
+            return to;
+         }
 };
