47d4e6eafd6a2483f14464346fa9d27da32ecced
NikhilSajan95 <nikhilputhenpurakal@gmail.com>
2026-06-07 00:20:14 +0530

    Time: 112 ms (5.31%), Space: 64.6 MB (5.75%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -3,20 +3,21 @@
  * @return {number[]}
  */
 var leftRightDifference = function(nums) {
-    leftSum = 0
-    rightSum = 0
-    leftArray = []
-    rightArray = []
-    answer = []
-    n = nums.length
-    for(i=0; i<n; i++){
-        leftSum += nums[i-1] ?? 0
-        leftArray.push(leftSum)
-        rightSum += nums[n-i] ?? 0
-        rightArray.unshift(rightSum)
+    const n = nums.length;
+    const ans = new Array(n);
+
+    let leftSum = 0;
+    for (let i = 0; i < n; ++i) {
+        ans[i] = leftSum;
+        leftSum += nums[i];
     }
-    for(i=0; i<n; i++){
-        answer.push(Math.abs(leftArray[i] - rightArray[i]))
+    console.log(ans)
+    let rightSum = 0;
+    for (let i = n - 1; i >= 0; --i) {
+        ans[i] = Math.abs(ans[i] - rightSum);
+        console.log(ans[i])
+        rightSum += nums[i];
     }
-    return answer
+    console.log(ans)
+    return ans;
 };
