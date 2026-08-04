4d0a949f77926782afd353fbf7536de2d1df22ad
NikhilSajan95 <nikhilputhenpurakal@gmail.com>
2026-08-05 01:48:22 +0530

    Time: 2 ms (87.69%), Space: 58.9 MB (31.54%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    const ans = [];
+
+    for (let i = 1; i < nums.length; i++) {
+        for (let x = nums[i - 1] + 1; x < nums[i]; x++) {
+            ans.push(x);
+        }
+    }
+
+    return ans;
+};
