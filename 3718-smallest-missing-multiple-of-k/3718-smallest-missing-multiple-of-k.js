9b95e596aa17f9092dd0e982f6a04ea6affedf10
NikhilSajan95 <nikhilputhenpurakal@gmail.com>
2026-09-05 03:34:06 +0530

    Time: 0 ms (100%), Space: 55.7 MB (63.16%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var missingMultiple = function(nums, k) {
+    const set = new Set(nums);
+
+    let multiple = k;
+
+    while (set.has(multiple)) {
+        multiple += k;
+    }
+
+    return multiple;
+};
