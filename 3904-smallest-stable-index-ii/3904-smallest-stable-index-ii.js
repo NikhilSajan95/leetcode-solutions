a08cb6be5620138117fbce5510085308345dad6d
NikhilSajan95 <nikhilputhenpurakal@gmail.com>
2026-09-07 01:32:46 +0530

    Time: 16 ms (62.3%), Space: 78.2 MB (46.93%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var firstStableIndex = function(nums, k) {
+    let n = nums.length;
+
+    let min = new Array(n);
+
+    min[n - 1] = nums[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        min[i] = Math.min(nums[i], min[i + 1]);
+    }
+
+    let max = 0;
+
+    for (let i = 0; i < n; i++) {
+
+        max = Math.max(max, nums[i]);
+
+        if ((max - min[i]) <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+};
